990e2bcaf11ed2215a0836e1afec3d721c15b2a5
Saikumar <gv.saikumar.97@gmail.com>
2021-09-12 12:59:00 +0530

    Removed react-router-dom unwanted dependency

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Education from "./components/Education/Education";
 import Experience from "./components/Experience/Experience";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* className="sm:px-8 sm:py-8 xs:px-5 xs:py-10 lg:px-12 lg:py-4" */}
-      <Router>
-        <Navbar />
-      </Router>
+      <Navbar />
       <div>
         <About />
         <Education />
